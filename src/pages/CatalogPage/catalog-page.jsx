4ecc5718ba7b1e29cfc68f2cd9478bf10c1af8f3
unknown <home_dv@mail.ru>
2023-02-27 21:26:28 +0300

    Finish task 2

---
+++ src/pages/CatalogPage/catalog-page.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import CardList from "../../components/CardList/card-list"
+import Sort from "../../components/Sort/sort"
+import Spinner from "../../components/Spinner"
+import { CardContext } from "../../context/cardContext";
+
+const tabs = [
+	{
+	  id: "cheap",
+	  title: "Сначала дешёвые",
+	},
+	{
+	  id: "low",
+	  title: "Сначала дорогие",
+	},
+	{
+	  id: "sale",
+	  title: "По скидке",
+	},
+  ];
+
+export const CatalogPage = () => {
+    const {cards} = useContext(CardContext);
+    return (
+        <>
+            <Sort tabs={tabs}/>
+            <div className='content__cards'>
+                <CardList cards={cards}/>
+            </div>
+        </>
+    )
+}
